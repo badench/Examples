66b062885103206261d2ce441462642bf6c92c2a
Connor Bade <cbade@proofpoint.com>
2021-09-28 07:58:23 -0700

    initial commit

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
